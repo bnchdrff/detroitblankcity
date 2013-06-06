82e1d592fafbb9332ed533bf3eaacca4431c8721
Benjamin Chodoroff <ben@theworkdept.com>
2013-06-05 22:43:40 -0400

    remove footer autohide, add meta tags and images

--- js/js.js
+++ js/js.js
@@ -2,16 +2,6 @@
 
 $(document).ready(function() {
 
-// footer
-var footer_hidden = { height: '1px' };
-var footer_shown = { height: '20px' };
-$('#links').animate(footer_hidden);
-$('#links').hover(function() {
-  $('#links').animate(footer_shown);
-}, function() {
-  $('#links').animate(footer_hidden);
-});
-
 // page state
 $('#e1, #e2').hide();
 $('#intro').click(function(ev) {
@@ -22,7 +12,7 @@ $('#intro').click(function(ev) {
 var goto_e = function(episode) {
   $('#intro, .e').hide();
   $('#e' + episode).show();
-  document.title = 'DETROIT BLANK CITY | EPISODE ' + episode;
+  document.title = 'DETROIT (BLANK CITY) | EPISODE ' + episode;
 };
 
 $('#gotoe1').click(function(ev) {
